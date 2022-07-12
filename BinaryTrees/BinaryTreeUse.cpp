b76df36c1d9be53b3fce8a5a97f154f6338d7036
thisisrv <rishabh.verma732@gmail.com>
2022-07-12 14:31:45 +0530

    Added files

--- BinaryTrees/BinaryTreeUse.cpp
+++ BinaryTrees/BinaryTreeUse.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<queue>
+#include<climits>
 #include "BinaryTreeNode.h"
 
 using namespace std;
@@ -205,7 +206,224 @@ void mirrorBinaryTree(BinaryTreeNode<int>* root) {
 
 }
 
+void inorder(BinaryTreeNode<int>* root){
 
+    //Base case
+    if(root == NULL)
+        return;
+
+    //small calc and rec call
+    inorder(root -> left);
+    cout << root -> data << " ";
+    inorder(root -> right);
+}
+
+void preorder(BinaryTreeNode<int>* root){
+
+    //Base case
+    if(root == NULL)
+        return;
+
+    //small calc and rec call
+    cout << root -> data << " ";
+    preorder(root -> left);
+    preorder(root -> right);
+}
+
+void postorder(BinaryTreeNode<int>* root){
+
+    //Base case
+    if(root == NULL)
+        return;
+
+    //small calc and rec call
+    postorder(root -> left);
+    postorder(root -> right);
+    cout << root -> data << " ";
+}
+
+//*******************************************************
+BinaryTreeNode<int>* buildTreehelper(int *preorder, int *inorder, int prS, int prE, int inS, int inE) {
+
+    //Base case
+    //if array are empty
+    if(inS > inE)
+        return NULL;
+
+    //Create root node
+    int rootData = preorder[prS];
+
+    //calculate root index
+    int rootIndex = -1;
+
+    for(int i = inS; i <= inE; i++){
+        if(inorder[i] == rootData){
+            rootIndex = i;
+            break;
+        }
+    }
+
+    int leftInorderStart = inS;
+    int leftInorderEnd = rootIndex - 1;
+    int leftPreorderStart = prS + 1;
+    int leftPreorderEnd = leftInorderEnd - leftInorderStart + leftPreorderStart;
+    int rightInorderStart = rootIndex + 1;
+    int rightInorderEnd = inE;
+    int rightPreorderStart = leftPreorderEnd + 1;
+    int rightPreorderEnd = prE;
+
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+
+    root -> left = buildTreehelper(preorder, inorder, leftPreorderStart, leftPreorderEnd, leftInorderStart, leftInorderEnd);
+    root -> right = buildTreehelper(preorder, inorder, rightPreorderStart, rightPreorderEnd, rightInorderStart, rightInorderEnd);
+ 
+
+    return root;
+
+}
+
+BinaryTreeNode<int>* buildTree(int *preorder, int preLength, int *inorder, int inLength){
+
+
+    BinaryTreeNode<int>* root = buildTreehelper(preorder, inorder, 0, preLength - 1, 0, inLength - 1);
+
+    return root;
+}
+
+
+pair<int, int> heightDiameter(BinaryTreeNode<int>* root){
+
+    //base case
+    if(root == NULL){
+
+        //create pair with height = 0 and diameter = 0;
+        //first = height and second  = diameter
+        pair<int, int> p;
+        p.first = 0;
+        p.second = 0;
+        return p;
+    }
+
+    //rec call
+    pair<int, int> leftAns = heightDiameter(root -> left);
+    pair<int, int> rightAns = heightDiameter(root -> right);
+
+    //small calc
+    int leftChildheight = leftAns.first;
+    int leftChilddiameter = leftAns.second;
+    int rightChildheight = rightAns.first;
+    int rightChilddiameter = rightAns.second;
+
+    //create pair
+    int height = 1 + max(leftChildheight, rightChildheight);
+    int diameter = max(leftChildheight + rightChildheight, max(leftChilddiameter, rightChilddiameter));
+    pair<int, int> p;
+    p.first = height;
+    p.second = diameter;
+
+    return p;
+}
+
+pair<int, int> getMinAndMax(BinaryTreeNode<int> *root) {
+	// Write your code here
+
+    //base case
+    if(root == NULL){
+
+        pair<int, int> p;
+        p.first = INT_MAX;
+        p.second = INT_MIN;
+
+        return p;
+    }
+
+
+    //rec call
+    pair<int, int> leftAns = getMinAndMax(root -> left);
+    pair<int, int> rightAns = getMinAndMax(root -> right);
+
+    //small calc
+    int leftMin = leftAns.first;
+    int leftMax = leftAns.second;
+    int rightMin = rightAns.first;
+    int rightMax = rightAns.second;
+
+    int minimum = min(root -> data, min(leftMin, rightMin));
+    int maximim = max(root -> data, max(leftMax, rightMax));
+
+    //create pair
+    pair<int, int> p;
+    p.first = minimum;
+    p.second = maximim;
+
+    return p;
+}
+
+
+//******************************************************************************************
+//ASSIGNMENT
+int getSum(BinaryTreeNode<int>* root) {
+    // Write your code here
+    //base case
+    if(root == NULL)
+        return 0;
+
+    //Rec call
+    int leftsum = getSum(root -> left);
+    int rightsum = getSum(root -> right);
+
+    //small calc
+    return root -> data + leftsum + rightsum;
+}
+
+pair<int, bool> isBalancedHelper(BinaryTreeNode<int>* root){
+
+    //base case
+    //root is NULL
+    if(root == NULL){
+        pair<int, bool> p;
+        p.first = 0;
+        p.second = true;
+
+        return p;
+    }
+
+    //Rec call
+    pair<int, bool> left = isBalancedHelper(root -> left);
+    pair<int, bool> right = isBalancedHelper(root -> right);
+
+
+    //small calc
+    pair<int, bool> p;
+
+    p.first = 1 + max(left.first, right.first);
+
+    //check for balanced
+    if(left.second == false || right.second == false)
+        p.second = false;
+    
+    else{
+
+        //if both are balanced
+        if(left.first - right.first >= -1 && left.first - right.first <= 1)
+            p.second = true;
+        
+        else
+            p.second = false;
+    }
+
+    return p;
+}
+
+bool isBalanced(BinaryTreeNode<int> *root) {
+	// Write your code here
+
+    pair<int, bool> p = isBalancedHelper(root);
+
+    return p.second;
+}
+
+//*******************************************************
 // 1 2 3 4 5 6 7 -1 -1 -1 -1 8 9 -1 -1 -1 -1 -1 -1
 //
 int main(){
@@ -214,19 +432,41 @@ int main(){
     // BinaryTreeNode<int> *node1 = new BinaryTreeNode<int>(2);
     // BinaryTreeNode<int> *node2 = new BinaryTreeNode<int>(3);
 
-    BinaryTreeNode<int> *root = takeInputLevelWise();
+    // BinaryTreeNode<int> *root = takeInputLevelWise();
 
-    printLevelWise(root);
+    // printLevelWise(root);
 
     // cout << countNodes(root) << endl;
     
     // cout << isNodePresent(root, 7) << endl;
     // cout << height(root) << endl;
 
-    mirrorBinaryTree(root);
+    // mirrorBinaryTree(root);
 
-    printLevelWise(root);
-    //delete tree
+    // printLevelWise(root);
+    
+    // inorder(root);
+    // preorder(root);
+
+    // int size;
+    // cin >> size;
+    // int *pre = new int[size];
+    // int *in = new int[size];
+    // for (int i = 0; i < size; i++) cin >> pre[i];
+    // for (int i = 0; i < size; i++) cin >> in[i];
+    // BinaryTreeNode<int> *root = buildTree(pre, size, in, size);
+    // printLevelWise(root);
+    
+    BinaryTreeNode<int> *root = takeInputLevelWise();
+
+    // pair<int, int> p = heightDiameter(root);
+    // pair<int, int> p = getMinAndMax(root);
+
+    // cout << "Min: " << p.first << endl;
+    // cout << "Max: " << p.second << endl;
+
+    cout << isBalanced(root) << endl;
+    //delete tree   
     delete root;
     
 }
